3d3774af539179baeb8162fa448afc6cf9eabf21
John Kamau <johnwaweru092@gmail.com>
2024-08-01 19:00:47 +0300

    First commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-950">
+      <Text className="text-4xl text-slate-100">Aora!</Text>
+    </View>
+  );
+};
+
+export default Home;
