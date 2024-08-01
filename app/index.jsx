b8605014df8f14034bdacfb2cf891f8ef56bbe8e
John Kamau <johnwaweru092@gmail.com>
2024-08-01 19:56:17 +0300

    Configured inner screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const Home = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-950">
-      <Text className="font-pregular text-4xl text-slate-100">Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-slate-950 p-5">
+      <Text className="font-pregular text-6xl text-slate-100 pb-2">Aora!</Text>
+      <Text className="font-plight text-lg text-slate-100 text-center">
+        Welcome to Aora! The best video sharing App
+      </Text>
+      <Link href="/home" style={{ color: "white" }}>
+        Home
+      </Link>
     </View>
   );
 };
